baff28b2bc79c6cbe648e071c2f7548c86ed3bf9
Gundel818 <adrian.gundelwein@etu.u-bordeaux.fr>
2024-03-02 15:12:41 +0100

    add enumeration of files in input dir

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from converter import Converter
+import os
 
-fullfpath = input("Entrez le chemin vers le fichier csv exporté d'EA : ")
+dir = os.getcwd() + "/input"
 
-converter = Converter(fullfpath)
+files = {}
+for i, file in enumerate(os.listdir(dir), start=1):
+    if file.endswith('.csv'):
+        files[i] = file
+        print(f"{i}. {file}")
+
+# Demander à l'utilisateur de saisir le numéro du fichier désiré
+numero_fichier = int(input("Entrez le numéro du fichier que vous souhaitez sélectionner : "))
+
+# Vérifier si le numéro de fichier est valide
+if numero_fichier in files:
+    filename = files[numero_fichier]
+    print("Vous avez sélectionné le fichier :", filename)
+else:
+    print("Numéro de fichier invalide.")
+    exit(1)
+
+# Concaténer le chemin du dossier avec le nom du fichier saisi
+pathfilename = os.path.join(dir, filename)
+
+converter = Converter(pathfilename)
 converter.convertToNewFile()
 
 
