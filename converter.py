a8796131ac6781085eec38aded8dc225abcfd891
Gundel818 <adrian.gundelwein@etu.u-bordeaux.fr>
2024-03-02 16:40:45 +0100

    fix wrong lon/lat with help of ID file

--- converter.py
+++ converter.py
@@ -1,13 +1,14 @@
 import pandas as pa
 from datetime import datetime
 
-class Converter:
+class Converterv2:
 
-    def __init__(self, inputfile : str):
-        self.__inputf = inputfile
-        self.__df = pa.read_csv(inputfile, header=0, sep=';', index_col=False)
+    def __init__(self, cellfile : str, idfile : str):
+        self.__df = pa.read_csv(cellfile, header=0, sep=';', index_col=False)
         self.__df.set_index('ID', inplace=True)
+        self.__id_df = pa.read_csv(idfile, header= 0, sep=';')
         self.__ope = ""
+
 
     def removeUselessColumnsandData(self):
         df = self.__df
@@ -18,21 +19,33 @@ class Converter:
         # Garder uniquement lignes sans 'Données insuffisantes' dans colonne 'NOM'
         df = df.dropna(subset=['NOM'])
         df = df[df['NOM'] != 'Données insuffisantes']
+        self.__df = df
+
+    def mergingIdinCells(self):
+        df = self.__df
+        id_df = self.__id_df
+        
+        df = df.merge(id_df[['eNB', 'Lat']], left_on='eNB', right_on='eNB', how='right')
+        df = df.merge(id_df[['eNB', 'Lon']], left_on='eNB', right_on='eNB', how='right')
+        df = df.drop(columns=['LAT', 'LON'])
 
         self.__df = df
 
     def renameAndReorderColumns(self):
+        # Récup les bonnes coordonées depuis le fichier d'identifications pour remplacer sur celui des cellules
+        self.mergingIdinCells()
+        
         df = self.__df
 
-        # Renommage colonnes
-        df = df.rename(columns={"eNB": "ECellID", "NOM" : "CellName", "LAT" : "Latitude", "LON" : "Longitude", "ARFCN" : "EARFCN"})
+        # Renommage colonnes                                            
+        df = df.rename(columns={"eNB": "ECellID", "NOM" : "CellName", "Lat" : "Latitude", "Lon" : "Longitude", "ARFCN" : "EARFCN"})
 
         ope = input("Quel opérateur ? (Orange, ByTel, ..) ")
         # TODO : mettre en miniscule l'input et les valeurs de la colonne OP pour éviter problèmes
         # TODO : Try except
         self.setOPE(ope)
         df = df.loc[df['OP'] == ope]
-        
+
         # Calcul ECellID
         df['ECellID'] = (df['ECellID'] * 256) + df['CID']
         df = df.sort_values(by='ECellID')
